969c97e2c584cdc642be14beeb63661ff2265476
Dick Balaska <dick@buckosoft.com>
2019-08-26 00:18:12 -0400

    Disable debug

--- client/src/main/java/net/gegy1000/slyther/client/recording/MessageSlytherGameFinished.java
+++ client/src/main/java/net/gegy1000/slyther/client/recording/MessageSlytherGameFinished.java
@@ -6,7 +6,6 @@ package net.gegy1000.slyther.client.recording;
 
 import net.gegy1000.slyther.client.db.GameStatistic;
 import net.gegy1000.slyther.network.MessageByteBuffer;
-import net.gegy1000.slyther.util.Log;
 
 /** Write the statistics at the end of the record file once the game is over.<br>
  * ReplayMan reads this metadata to display on its screen.
@@ -31,6 +30,6 @@ public class MessageSlytherGameFinished extends TimedMessage {
 		length = bb.position();
 		payload = new byte[length];
 		System.arraycopy(bb.array(), 0, payload, 0, length);
-		Log.info("bb = {}", payload);
+		//Log.info("bb = {}", payload);
 	}
 }
